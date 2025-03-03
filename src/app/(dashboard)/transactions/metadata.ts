e0949f12cfc114184d9bce924cf4e78bfee425b2
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2025-03-03 22:30:33 +0800

    add: transactions page

---
+++ src/app/(dashboard)/transactions/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "View Transactions | Scrooge Bank CRM System",
+  description: "View client transactions in the Scrooge Bank CRM system",
+};

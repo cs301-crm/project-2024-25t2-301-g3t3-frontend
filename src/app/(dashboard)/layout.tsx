e9aa8f424549934b59baeb3b479f0d94a70398e7
adrian <adrianjh2001@gmail.com>
2025-04-12 19:05:13 +0800

    Added RouteGuard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "../../components/layout/header";
 import { DynamicSidebar } from "../../components/layout/DynamicSidebar";
 import { ClientProvider } from "@/contexts/client-context"; 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import RouteGuard from "@/components/RouteGuard";
 
 const queryClient = new QueryClient();
 export default function DashboardLayout({
@@ -13,6 +14,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <RouteGuard>
       <QueryClientProvider client={queryClient}>
         <ClientProvider>
         <div className="flex flex-col h-screen bg-slate-50">
@@ -26,5 +28,6 @@ export default function DashboardLayout({
       </div>
         </ClientProvider>
       </QueryClientProvider>
+      </RouteGuard>
   );
 }
---
+++ src/components/RouteGuard.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@/contexts/user-context';
+import { Loader2 } from 'lucide-react';
+
+interface RouteGuardProps {
+  children: React.ReactNode;
+}
+
+export default function RouteGuard({ children }: RouteGuardProps) {
+  const { user, loading } = useUser();
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true); 
+  }, []);
+
+  useEffect(() => {
+    if (mounted && !loading && (!user || !user.userId )) {
+      router.push('/login');
+    }
+  }, [mounted, loading, user, router]);
+
+  if (!mounted || loading || !user || !user.userId) return (
+    <div className="flex items-center justify-center h-screen">
+        <Loader2 className="h-10 w-10 animate-spin text-muted-foreground" />
+    </div>
+  )
+
+  return <>{children}</>;
+}
